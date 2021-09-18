15eb021c65dd45d61509f6d7b5cfc29779dfbbe2
PabloLec <pablo.lecolinet@gmail.com>
2021-09-18 10:13:03 +0200

    Loading optimization test

--- src/js/face/face.js
+++ src/js/face/face.js
@@ -1,6 +1,9 @@
-const app = new SpeRuntime.Application();
-app.start("/src/scene/scene.json");
-var elem = document.createElement("div");
-elem.style.cssText = "width:0%;height:0%;display:none";
-elem.setAttribute("id", "face-loaded");
-document.body.appendChild(elem);
+async function loadApp() {
+  const app = new SpeRuntime.Application();
+  await app.start("/src/scene/scene.json");
+  var elem = document.createElement("div");
+  elem.style.cssText = "width:0%;height:0%;display:none";
+  elem.setAttribute("id", "face-loaded");
+  document.body.appendChild(elem);
+}
+loadApp();
